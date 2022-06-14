5c4a2cacefe0687ae85d990e53cd1a283397d197
walidark <mohwalidas2@gmail.com>
2022-06-14 08:46:45 +0800

    update index

--- resources/views/pages/bupot/bupot-pph21/index.blade.php
+++ resources/views/pages/bupot/bupot-pph21/index.blade.php
@@ -98,6 +98,12 @@
                                         </thead>
                                         <tbody>
                                         </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th colspan="7" style="text-align:right">Total</th>
+                                                <th></th>
+                                            </tr>
+                                        </tfoot>
                                     </table>
                                 </div>
                             </div>
